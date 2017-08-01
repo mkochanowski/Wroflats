8cd804577fcb277f8546ea2ba5fec0ffa47a2cdc
Marek Kochanowski <marek@kochanow.ski>
2017-08-01 13:49:12 +0200

    Hard code price into query

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ class SubmissionsIndex(Resource):
         #rv = None
         if rv is None:
             with g.db.cursor() as cursor:
-                sql = "SELECT * FROM `wroflats_submissions` WHERE `deactivated`=0 {}ORDER BY `rating` DESC LIMIT {}30".format(categories, pagination)
+                sql = "SELECT * FROM `wroflats_submissions` WHERE `deactivated`=0 AND `price` <= 2200 {}ORDER BY `rating` DESC LIMIT {}30".format(categories, pagination)
                 cursor.execute(sql)
                 result = cursor.fetchall()
                 cursor.close()
