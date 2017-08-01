cb7083a7e7202cb0c53dfb4e1a94452c0e611521
Marek Kochanowski <marek@kochanow.ski>
2017-08-01 10:16:07 +0200

    Update database connections and error logging

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import config
 import json
 import datetime
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, g
 from flask_restful import Resource, Api
 from json import dumps
 
 print("app.py initialize")
-connection = config.connection
+#connection = config.connection
 
 app = Flask(__name__)
 api = Api(app)
@@ -16,6 +16,38 @@ def output_json(data, code, headers=None):
     resp = make_response(json.dumps(data, default=datetime_handler), code)
     resp.headers.extend(headers or {})
     return resp
+
+@app.before_request
+def before_request():
+    print('executed connect')
+    g.db = config.connect()
+
+@app.teardown_request
+def teardown_request(exception):
+    if hasattr(g, 'db'):
+        print('executed close')
+        g.db.close()
+
+if not app.debug:
+    import logging
+    from logging.handlers import SMTPHandler
+    from logging import Formatter
+    mail_handler = SMTPHandler('127.0.0.1',
+                               'root@wroflats.xememah.com',
+                               'askxememah@gmail.com', 'Wroflats has failed Failed')
+    mail_handler.setFormatter(Formatter('''
+    Message type:       %(levelname)s
+    Location:           %(pathname)s:%(lineno)d
+    Module:             %(module)s
+    Function:           %(funcName)s
+    Time:               %(asctime)s
+
+    Message:
+
+    %(message)s
+    '''))
+    mail_handler.setLevel(logging.ERROR)
+    app.logger.addHandler(mail_handler)
 
 def datetime_handler(x):
     if isinstance(x, datetime.datetime):
@@ -30,7 +62,7 @@ class Index(Resource):
 
 class SubmissionsIndex(Resource):
     def get(self):
-        with connection.cursor() as cursor:
+        with g.db.cursor() as cursor:
             sql = "SELECT * FROM `wroflats_submissions` WHERE `deactivated`=0 ORDER BY `rating` DESC LIMIT 60"
             cursor.execute(sql)
             result = cursor.fetchall()
@@ -40,7 +72,7 @@ class SubmissionsIndex(Resource):
 
 class Submission(Resource):
     def get(self, hash):
-        with connection.cursor() as cursor:
+        with g.db.cursor() as cursor:
             sql = "SELECT * FROM `wroflats_submissions` WHERE `hash`=%s LIMIT 1"
             cursor.execute(sql, hash)
             result = cursor.fetchone()
@@ -50,7 +82,7 @@ class Submission(Resource):
 
 class Token(Resource):
     def get(self, token):
-        with connection.cursor() as cursor:
+        with g.db.cursor() as cursor:
             sql = "SELECT * FROM `wroflats_tokens` WHERE `token`=%s ORDER BY `created_date` DESC LIMIT 1"
             cursor.execute(sql, token)
             result = cursor.fetchone()
@@ -65,7 +97,7 @@ class Token(Resource):
 class Authorize(Resource):
     def post(self):
         pin = request.json['pin']
-        with connection.cursor() as cursor:
+        with g.db.cursor() as cursor:
             sql = "SELECT `id` FROM `wroflats_users` WHERE `access_code`=%s LIMIT 1"
             cursor.execute(sql, pin)
             result = cursor.fetchone()
@@ -83,7 +115,7 @@ class Authorize(Resource):
                     token = config.rand_str(20)
                     sql = "INSERT INTO `wroflats_tokens` (`user_id`, `token`) VALUES (%s, %s)"
                     cursor.execute(sql, (user_id, token))
-                    connection.commit()       
+                    g.db.commit()       
                 cursor.close()
 
                 return {'status': 'ok', 'token': token }
@@ -98,4 +130,3 @@ api.add_resource(Index, '/')
 
 if __name__ == '__main__':
     app.run(debug=True)
-    connection.close()
--- config.py
+++ config.py
@@ -9,14 +9,18 @@ if os.path.isfile('.env'):
     dotenv_path = join(dirname(__file__), '.env')
     load_dotenv(dotenv_path)
 
-# Connect to the database
-connection = pymysql.connect(host=os.environ['DB_HOST'],
+def rand_str(length):
+    return ''.join(
+        random.choice(string.ascii_uppercase) for i in range(length))
+
+def connect():
+    connection = pymysql.connect(host=os.environ['DB_HOST'],
                              user=os.environ['DB_USER'],
                              password=os.environ['DB_PASS'],
                              db=os.environ['DB_NAME'],
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
+    return connection
 
-def rand_str(length):
-    return ''.join(
-        random.choice(string.ascii_uppercase) for i in range(length))
+# Connect to the database
+connection = connect()
