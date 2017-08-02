e397ad1c3a461b5f5bca96b1075099976d282bcd
Marek Kochanowski <marek@kochanow.ski>
2017-08-03 00:03:14 +0200

    Update duplication removal algorithm

--- duplicate_punisher.py
+++ duplicate_punisher.py
@@ -4,30 +4,47 @@ import datetime
 connection = config.connect()
 
 with connection.cursor() as cursor:
-	#sql = "SELECT * FROM `wroflats_submissions` WHERE `id`=2429" #debug
-	sql = "SELECT * FROM `wroflats_submissions` WHERE `rating_modifier`=1 AND `full_scrap`=1 ORDER BY `rating` DESC LIMIT 1000"
-	cursor.execute(sql)
-	result = cursor.fetchall()
-	cursor.close()
-	
-	index = 0
-	all_count = 0
-	for item in result:
-		index += 1
-		print("{}. id: {}, current rating: {}, rating modifier: {} ".format(index, item['id'], item['rating'], item['rating_modifier']))
+    #sql = "SELECT * FROM `wroflats_submissions` WHERE `id`=2429" #debug
+    sql = "SELECT * FROM `wroflats_submissions` WHERE `rating_modifier`=1 AND `full_scrap`=1 ORDER BY `rating` DESC LIMIT 1000"
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    cursor.close()
+    
+    index = 0
+    all_count = 0
+    all_removed = 0
+    for item in result:
+        index += 1
+        print("{}. id: {}, current rating: {}, rating modifier: {} ".format(index, item['id'], item['rating'], item['rating_modifier']))
 
-		# check for duplicates
-		with connection.cursor() as cursor:
-			sql = "SELECT `id`, `title` FROM `wroflats_submissions` WHERE `short_desc` LIKE %s AND `rating_modifier`=1 AND `id` <> %s"
-			cursor.execute(sql, ("%"+item['short_desc']+"%", item['id']))
-			duplicates = cursor.fetchall()
+        # check if was removed
+        with connection.cursor() as cursor:
+            sql = "SELECT `id`, `title` FROM `wroflats_submissions` WHERE `short_desc` LIKE %s AND `rating_modifier`=0 AND `rating`=0 AND `id` <> %s"
+            cursor.execute(sql, ("%"+item['short_desc']+"%", item['id']))
+            removed = cursor.fetchall()
+            
+            if removed:
+                sql = "UPDATE `wroflats_submissions` SET `rating_modifier`=0, `rating`=0 WHERE `short_desc` LIKE %s AND `rating_modifier`=1"
+                cursor.execute(sql, ("%"+item['short_desc']+"%"))
+                connection.commit()
+                all_removed += len(removed)
+                print('found removed submission!')
+                print(removed)
+            cursor.close()
 
-			if duplicates:
-				sql = "UPDATE `wroflats_submissions` SET `rating_modifier`=0.1 WHERE `short_desc` LIKE %s AND `rating_modifier`=1 AND `id` <> %s"
-				cursor.execute(sql, ("%"+item['short_desc']+"%", item['id']))
-				connection.commit()
-				all_count += len(duplicates)
+        # check for duplicates
+        with connection.cursor() as cursor:
+            sql = "SELECT `id`, `title` FROM `wroflats_submissions` WHERE `short_desc` LIKE %s AND `rating_modifier`=1 AND `id` <> %s"
+            cursor.execute(sql, ("%"+item['short_desc']+"%", item['id']))
+            duplicates = cursor.fetchall()
 
-			cursor.close()
-	print("All duplicates removed: {}".format(all_count))
+            if duplicates:
+                sql = "UPDATE `wroflats_submissions` SET `rating_modifier`=0.1 WHERE `short_desc` LIKE %s AND `rating_modifier`=1 AND `id` <> %s"
+                cursor.execute(sql, ("%"+item['short_desc']+"%", item['id']))
+                connection.commit()
+                all_count += len(duplicates)
+            cursor.close()
+        
+    print("All duplicates removed: {}, hidden: {}".format(all_count, all_removed))
+    
 connection.close()
